c1a1fa7524b5154494efe4cd46bb55ba523667ac
ushayenni <151708074+ushayenni@users.noreply.github.com>
2026-05-01 20:10:15 +0530

    Update restapp.py

--- restapp.py
+++ restapp.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
+
 app = Flask(__name__)
-@app.route(&#39;/greet&#39;, methods=[&#39;GET&#39;])
+
+@app.route('/greet', methods=['GET'])
 def greet():
-name = request.args.get(&#39;name&#39;, &#39;Student&#39;)
-return jsonify({&quot;message&quot;: f&quot;Hello {name}, welcome to REST API!&quot;})
-if __name__ == &#39;__main__&#39;:
-app.run(debug=True)
+    name = request.args.get('name', 'Student')
+    return jsonify({"message": f"Hello {name}, welcome to REST API!"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
